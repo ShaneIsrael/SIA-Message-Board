ac4da6785a6e7c67b3b52c4de5b5dfcbc02dbfc4
Shane Israel <sisrael@sofi.org>
2015-05-28 16:26:01 -0600

    added Task model, in-memory database config, and test

---
+++ test/TaskTest.java
@@ -0,0 +1,24 @@
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.running;
+
+import models.Task;
+
+import org.junit.Test;
+
+public class TaskTest {
+
+	@Test
+	public void create() {
+		running(fakeApplication(), new Runnable() {
+			public void run() {
+				Task task = new Task();
+				task.contents = "Write a test";
+				task.id = "something";
+				task.save();
+				assertThat(task.id).isNotNull();
+			}
+		});
+	}
+
+}
