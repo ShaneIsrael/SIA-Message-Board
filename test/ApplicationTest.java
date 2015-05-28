9f25f0523500a119a5fa3c76468b70208490c928
Shane Israel <sisrael@sofi.org>
2015-05-28 16:03:05 -0600

    change the view

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,19 +1,6 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.*;
 
 import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
 import play.twirl.api.Content;
 
 import static play.test.Helpers.*;
@@ -36,9 +23,9 @@ public class ApplicationTest {
 
     @Test
     public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
+        Content html = views.html.index.render("HELLO, WORLD");
         assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
+        assertThat(contentAsString(html)).contains("HELLO, WORLD");
     }
 
     @Test
@@ -47,7 +34,7 @@ public class ApplicationTest {
     	assertThat(status(result)).isEqualTo(OK);
     	assertThat(contentType(result)).isEqualTo("text/html");
     	assertThat(charset(result)).isEqualTo("utf-8");
-    	assertThat(contentAsString(result)).contains("hello, world");
+    	assertThat(contentAsString(result)).contains("HELLO, WORLD");
     }
 
 
