6e0ad718cbf5d74461a174b2b21fc0e1d0d33cdc
Shane Israel <sisrael@sofi.org>
2015-06-08 14:48:15 -0600

    Added the abilityh to change the text color by using preconfigured color commands or by entering your own hex color command

--- public/javascripts/refresh.js
+++ public/javascripts/refresh.js
@@ -16,11 +16,48 @@ function getBoard() {
 		  var items = [];
 		  for (ix in data) {
 			  var message = data[ix];
-			  $("#messages").append($("<ul>").text(message.contents));
+			  var colors = ["red", "green", "blue", "cyan", "pink", "purple", "yellow", "gray", "magenta"];
+			  var begin = message.contents.indexOf("#");
+			  var end = getPosition(message.contents, ":", 2);
+			  var hasColor = false;
+			  for(i in colors) {
+				  var color = colors[i];
+				  if (message.contents.indexOf(""+color+":") > -1) {
+					  message.contents = message.contents.replace(""+color+":","");
+					  var newFont = $("<font color='"+color+"'></font>").text(message.contents);
+					  var newUl = $("<ul></ul>");
+					  newUl.append(newFont);
+					  $("#messages").append(newUl);
+					  hasColor = true;
+					  break;
+				  }
+			  }
+			  if (hasColor == false) {
+				  if (begin > -1 && end > -1) {
+					  var hexColor = message.contents.substring(begin, end);
+					  if (hexColor.indexOf(" ") == -1) {
+						 if(/^#[0-9a-f]|[0-9A-F]{3,6}$/.test(hexColor)) {
+							 hasColor = true;
+							 message.contents = message.contents.replace(""+hexColor+":","");
+							  var newFont = $("<font color='"+hexColor+"'></font>").text(message.contents);
+							  var newUl = $("<ul></ul>");
+							  newUl.append(newFont);
+							  $("#messages").append(newUl);
+						 }
+					  }
+				  }
+			  }
+
+			  if (hasColor == false) {
+				  $("#messages").append($("<ul>").text(message.contents));
+			  }
+
 		  }
 	});
 }
-
+function getPosition(str, m, i) {
+	   return str.split(m, i).join(m).length;
+	}
 function submitHandler() {
 	// Attach a submit handler to the form
 	$( "#submitForm" ).submit(function( event ) {
