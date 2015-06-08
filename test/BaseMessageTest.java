b2138a6849215b46dda56cf75c3028bc18858f53
Shane Israel <sisrael@sofi.org>
2015-06-08 11:18:11 -0600

    Now have unit tests working completely using fakeApplication

--- test/BaseMessageTest.java
+++ test/BaseMessageTest.java
@@ -1,40 +1,52 @@
 import models.Message;
 import models.Register;
 
+import services.MessageService;
+
+import org.junit.runner.Computer;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 import java.util.List;
+import static play.test.Helpers.*;
 
 import static org.fest.assertions.Assertions.assertThat;
 
 @ContextConfiguration(classes={TestAppConfig.class, TestDatabaseConfig.class})
-public class BaseMessageTest {
+public class BaseMessageTest extends AbstractTransactionalJUnit4SpringContextTests {
 
     @Autowired
-    private MessageServiceTest msgService;
+    private MessageService msgService;
 
     @Test
     public void testMessage() {
-        Message message = new Message();
-        message.setContents("This is a test message");
-//        msgService.test();
-//        msgService.addMessage(message);
-//        assertThat(message.getId()).isNotNull();
-//        List<Message> messages = msgService.getMessages();
-//        assertThat(messages.size()).isEqualTo(1);
+    	running(fakeApplication(), new Runnable() {
+    	      public void run() {
+    	          Message message = new Message();
+    	          message.setContents("This is a test message");
+    	          msgService.addMessage(message);
+    	          assertThat(message.getId()).isNotNull();
+    	          List<Message> messages = msgService.getMessages();
+    	          assertThat(messages.size()).isEqualTo(1);
+    	       }
+    	   });
     }
 
     @Test
     public void testRegister() {
-    	Register register = new Register();
-    	register.setPhoneNumber("+4065550123");
-//    	msgService.registerNumber(register);
-//    	assertThat(register.getId()).isNotNull();
-//    	List<Register> users = msgService.getRegisteredNumbers();
-//    	assertThat(users.size()).isEqualTo(1);
+    	running(fakeApplication(), new Runnable() {
+  	      public void run() {
+  	    	Register register = new Register();
+  	    	register.setPhoneNumber("+4065550123");
+  	    	msgService.registerNumber(register);
+  	    	assertThat(register.getId()).isNotNull();
+  	    	List<Register> users = msgService.getRegisteredNumbers();
+  	    	assertThat(users.size()).isEqualTo(1);
+  	       }
+  	   });
     }
 
 }
