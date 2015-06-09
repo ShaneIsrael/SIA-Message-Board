70295f7b8a3f43570bc84fd28873f0f81ce4c214
Shane Israel <sisrael@sofi.org>
2015-06-09 14:16:27 -0600

    Fixed unit tests to now work in production

--- app/configs/AppConfig.java
+++ app/configs/AppConfig.java
@@ -1,9 +1,38 @@
 package configs;
-
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan({"controllers","services"})
 public class AppConfig {
+
+	@Bean
+    public static PropertyPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertyPlaceholderConfigurer() {
+            @Override
+            protected String resolvePlaceholder(String placeholder, Properties props) {
+                Properties p = new Properties();
+                try {
+                    p.load(FileUtils.openInputStream(new File("conf/application.conf")));
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                // When property is not found, returning null will cause an IllegalArgumentException stating the parameter that is
+                // missing in @Value.
+                String result = p.getProperty(placeholder);
+                if (result == null) {
+                    result = "";
+                }
+                return result.replaceAll("\"", "");
+            }
+        };
+    }
 }
--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
   javaCore,
-  javaWs % "test",
+  javaWs,
   "org.webjars" % "bootstrap" % "3.3.4",
   "com.h2database" % "h2" % "1.4.181",
   "org.slf4j" % "slf4j-api" % "1.7.12",
@@ -16,6 +16,7 @@ libraryDependencies ++= Seq(
   "org.springframework" % "spring-aop" % "4.1.1.RELEASE",
   "org.springframework" % "spring-test" % "4.1.1.RELEASE" % "test",
   "org.hibernate" % "hibernate-entitymanager" % "4.3.6.Final",
+  "org.apache.commons" % "commons-io" % "1.3.2",
   "mysql" % "mysql-connector-java" % "5.1.35",
   "com.twilio.sdk" % "twilio-java-sdk" % "3.4.5"
 )
